89be67fe3deb0707d2b2e6c4628597c35035369b
Tomy Ismail <tomysmile@gmail.com>
2024-08-07 17:51:41 +0700

    add js button and calculation

--- rentals/rentals/doctype/ride_booking_item/ride_booking_item.py
+++ rentals/rentals/doctype/ride_booking_item/ride_booking_item.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2024, Thinkspedia and contributors
 # For license information, please see license.txt
 
-import frappe
+# import frappe
 from frappe.model.document import Document
 
-class RideOrder(Document):
-	def validate(self):
-		pass
+
+class RideBookingItem(Document):
+	pass
--- rentals/rentals/doctype/ride_order/ride_order.js
+++ rentals/rentals/doctype/ride_order/ride_order.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2024, Thinkspedia and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Ride Order", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Ride Order", {
+	refresh(frm) {
+    if (frm.doc.status !== "Accepted") {
+      frm.add_custom_button("Accept", () => { 
+        // status => Accepted
+        frm.set_value("status", "Accepted");
+        // save the form
+        frm.save();
+      })
+    }
+	},
+});
