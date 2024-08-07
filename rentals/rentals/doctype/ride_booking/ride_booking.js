a85043841c189204e03982be19b4ee9498b9727e
Tomy Ismail <tomysmile@gmail.com>
2024-08-07 19:09:50 +0700

    add calculated total amount in child table

--- rentals/rentals/doctype/ride_booking/ride_booking.js
+++ rentals/rentals/doctype/ride_booking/ride_booking.js
@@ -1,8 +1,23 @@
 // Copyright (c) 2024, Thinkspedia and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Ride Booking", {
-// 	refresh(frm) {
+frappe.ui.form.on("Ride Booking", {
+	refresh(frm) {
+		//
+	},
+});
 
-// 	},
-// });
+frappe.ui.form.on("Ride Booking Item", {
+	refresh(frm) {
+		//
+	},
+	distance(frm, cdt, cdn) {
+		let total_d = 0;
+		for(let item of frm.doc.items) {
+			total_d += item.distance;
+		}
+
+		const amount = frm.doc.rate * total_d;
+		frm.set_value("total_amount", amount);
+	}
+});
