81baa0ece10f5786afd4929204a92602157178d9
Tomy Ismail <tomysmile@gmail.com>
2024-08-09 18:42:09 +0700

    create unit test for full name

--- rentals/rentals/doctype/driver/test_driver.py
+++ rentals/rentals/doctype/driver/test_driver.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2024, Thinkspedia and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestDriver(FrappeTestCase):
-	pass
+	def test_full_name_correctly_set(self):
+		test_driver = frappe.new_doc("Driver")
+		test_driver.first_name = "John"
+		test_driver.last_name = "Doe"
+		test_driver.license_number = "XYZ12345"
+		test_driver.phone_number = "+6281299912345"
+		test_driver.save()
+
+		self.assertEqual(test_driver.full_name, "John Doe")
